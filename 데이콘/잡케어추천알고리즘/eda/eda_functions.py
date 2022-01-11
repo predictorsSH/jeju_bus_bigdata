6fc773cb8768439bdf7e204157fbe76dc318b70f
predictorsSH <sanghyun9082@gmail.com>
2022-01-11 14:04:12 +0900

    잡케어프로젝트 delete

--- 데이콘/잡케어추천알고리즘/eda/eda_functions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def bar_chart(x: pd.Series) :
-    x.value_counts().plot(kind='bar')
-    plt.title(x.name)
-    plt.ylabel('counts')
-    plt.show()
-
